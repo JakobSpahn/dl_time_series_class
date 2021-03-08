3000bec859e3276d33901cffa6d9a5c1cc295b79
Jakob Spahn <jakob@craalse.de>
2021-03-08 08:32:15 +0100

    init

---
+++ layers/reservoir.py
@@ -0,0 +1,85 @@
+import scipy as sp
+import numpy as np
+import tensorflow as tf
+from tqdm.auto import trange
+
+class Reservoir:
+    def __init__(self, units, n_in, IS, spectral_radius, connectivity, leaky, bias=False, activation='tanh',seed=1882517):
+        print('Initializing reservoir space...')
+        
+        self.units = units
+        self.n_in = n_in
+        self.IS = IS
+        self.spectral_radius = spectral_radius
+        self.connectivity = connectivity
+        self.leaky = leaky
+        self.bias = bias
+        self.activation=activation
+        
+        #Set Seed for Numpy:
+        rng = np.random.RandomState(seed)
+        
+        #Random Initialization of input weight values
+        self.W_in = 2*np.array(np.random.random(size=(units, n_in)))-1
+        
+        #Generate sparsity matrix
+        W_res_temp = sp.sparse.rand(self.units, self.units, self.connectivity) #(-> units x units), density of matrix = sparsity
+        
+        #Find one (k=1) eigenvalue and the corresponding eigenvector
+        vals, vecs = sp.sparse.linalg.eigsh(W_res_temp, k=1)
+        
+        #Init reservoir weight values
+        self.W_res = self.spectral_radius * W_res_temp / vals[0]
+        
+        #Init bias
+        if bias:
+            b_bound=1
+            self.b = 2 * b_bound * np.random.random(size=(self.units, 1))-b_bound
+        else:
+            self.b = 0
+    
+    def set_weights(self, data):
+        #print('Setting weights')
+
+        n_forget_steps = 0
+        num_samples = np.shape(data)[0]
+        num_frames = np.shape(data)[1]
+        
+        #Init weight template: num_samples * timestamps * units
+        weights = np.empty((num_samples,(num_frames-n_forget_steps), self.units), np.float32)
+        
+
+        #For every time series in the dataset:
+        for i_sample in (bar := trange(num_samples)):
+            series = data[i_sample]
+            #print('Create weights of %4d-th sample'%(i_sample)
+            collect_weights = np.zeros((num_frames-n_forget_steps, self.units))
+            x = np.zeros((self.units, 1))
+            
+            #inner = tqdm.tqdm(total=num_frames, desc='Stamp', position=1)
+            #Now apply weights for every timestamp
+            for t in range(num_frames):
+                u_t = np.asarray([series[t,:]]).T #Get every timestamp. Requires 3D shape to begin with
+                
+                #ToDo: Extend Dict
+                xUpd = {
+                    'tanh': lambda bias: np.tanh(np.dot(self.W_in, self.IS*u_t) + \
+                                                 np.dot(self.W_res.toarray(), x) + bias),
+                    
+                    'relu': lambda bias: tf.nn.relu(np.dot(self.W_in, self.IS*u_t) + \
+                                                    np.dot(self.W_res.toarray(), x) + bias).numpy()
+                }[self.activation](self.b)
+                
+                x = (1-self.leaky)*x + self.leaky*xUpd
+                if t >= n_forget_steps:
+                    collect_weights[t-n_forget_steps,:] = x.T
+                
+                
+            
+            collect_weights = np.asarray(collect_weights)
+            weights[i_sample] = collect_weights
+
+            bar.set_description("Setting Weights")
+        
+
+        return weights
---
+++ models/emn.py
@@ -0,0 +1,222 @@
+import tensorflow.keras as keras
+import tensorflow as tf
+import numpy as np
+import time
+
+from layers.reservoir import Reservoir
+
+from utils.utils import save_logs
+
+import matplotlib
+
+from tqdm.keras import TqdmCallback
+
+class Classifier_EMN:
+    def __init__(self, output_dir, nb_classes, verbose):
+        self.output_dir = output_dir
+        self.nb_classes = nb_classes
+        self.verbose = verbose
+        
+        #Hyperparameters ESN
+        #self.esn_config = {'units':32, 'connect':0.7,'IS':0.1,"spectral":0.9,'leaky':1}
+
+        self.units = 32
+        self.spectral = 0.9
+        self.IS = [0.1,1]
+        self.connectivity = [0.3,0.7]
+        self.leaky = 1
+
+        #Hyperparameters Convolutions
+        #self.conv_config = {'epoch':500,'batch':25,'ratio':[0.6,0.7]}
+        self.epoch = 500
+        self.batch = 25
+        self.ratio = [[0.1,0.2],[0.2,0.3],[0.3,0.4],[0.4,0.5],[0.5,0.6],[0.6,0.7],[0.7,0.8]]
+        
+        
+    def build_model(self, input_shape, nb_classes, len_series, ratio):
+        #ratio = self.conv_config['ratio']
+        nb_rows = [np.int(ratio[0]*len_series),np.int(ratio[1]*len_series)]
+        nb_cols = input_shape[2]
+        
+        input_layer = keras.layers.Input(input_shape)
+        
+        x_layer_1 = keras.layers.Conv2D(120, (nb_rows[0], nb_cols), kernel_initializer='lecun_uniform', activation='relu',
+                                       padding='valid', strides=(1,1), data_format = 'channels_last')(input_layer)
+        x_layer_1 = keras.layers.GlobalMaxPooling2D(data_format = 'channels_first')(x_layer_1)
+        
+        
+        
+        y_layer_1 = keras.layers.Conv2D(120, (nb_rows[1], nb_cols), kernel_initializer='lecun_uniform', activation='relu',
+                                       padding='valid', strides=(1,1), data_format = 'channels_last')(input_layer)
+        y_layer_1 = keras.layers.GlobalMaxPooling2D(data_format = 'channels_last')(y_layer_1)
+        
+        
+        
+        concat_layer = keras.layers.concatenate([x_layer_1, y_layer_1])
+
+        layer_2 = keras.layers.Dense(64, kernel_initializer='lecun_uniform', activation = 'relu')(concat_layer)
+
+        layer_3 = keras.layers.Dense(128, kernel_initializer='lecun_uniform', activation = 'relu')(layer_2)
+        layer_3 = keras.layers.Dropout(0.25)(layer_3)
+        
+        output_layer = keras.layers.Dense(nb_classes, kernel_initializer='lecun_uniform', activation='softmax')(layer_3)
+        
+        model = keras.models.Model(input_layer, output_layer)
+        
+        model.compile(loss='categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(learning_rate=0.001), metrics=['accuracy'])
+        
+        self.callbacks = [TqdmCallback(verbose=0)]
+        
+        
+        return model
+        
+    def reshape_shuffle(self, x_train, y_train, nb_samples, res_units, len_series):
+
+        #Generate template for train data
+        train_data = np.zeros((nb_samples, 1, len_series, res_units))
+        train_labels = np.zeros((nb_samples, self.nb_classes))
+
+        #Generate Shuffle template
+        L_train = [x_train for x_train in range(nb_samples)] #Array with size==samples, every value==index 
+        np.random.shuffle(L_train)
+        
+        #For every series -> shuffle train and labels
+        for m in range(nb_samples):
+            train_data[m,0,:,:] = x_train[L_train[m],:,:] 
+            train_labels[m,:] = y_train[L_train[m],:]
+        
+        return train_data, train_labels      
+    
+    def ff_esn(self, x_train, x_val, y_train, IS, connect):
+        units = self.units
+        spectral_radius = self.spectral
+        leaky = self.leaky
+        n_in = 1
+        
+        escnn = Reservoir(units, n_in, IS, spectral_radius, connect, leaky)
+        x_train = escnn.set_weights(x_train)
+        x_val = escnn.set_weights(x_val)
+
+        self.nb_samples_train = np.shape(x_train)[0]
+        self.nb_samples_val = np.shape(x_val)[0]
+        self.len_series = x_val.shape[1]
+
+        #Reshape test and train data. Train data is also shuffled
+        x_val = np.reshape(x_val,(self.nb_samples_val, self.len_series, self.units, 1))
+        x_train, y_train = self.reshape_shuffle(x_train, y_train, self.nb_samples_train, self.units, self.len_series) 
+
+        #From NCHW to NHWC
+        x_train = tf.transpose(x_train, [0, 2, 3, 1])
+        print('NHWC: {0}'.format(x_train.shape))
+        #print(x_train.shape)
+        
+        return x_train, x_val, y_train
+
+    def tune_esn(self, x_train_init, x_val_init, y_train_init, y_val):
+        final_IS = None
+        final_connect = None
+        x_train_final = None
+        x_val_final = None
+        y_train_final = None 
+        duration_final = None   
+        model_final = None
+        hist_final = None   
+
+        current_acc = 0.0
+
+        for IS in self.IS:
+            for connect in self.connectivity:
+                ratio = [0.1,0.2]
+
+                x_train, x_val, y_train = self.ff_esn(x_train_init, x_val_init, y_train_init, IS, connect)
+
+                #2. Build Model
+                input_shape = (self.len_series, self.units, 1)
+                model = self.build_model(input_shape, self.nb_classes, self.len_series, ratio)
+                if(self.verbose==True):
+                    model.summary()
+        
+        
+                #3. Train Model
+                batch = self.batch
+                epoch = self.epoch
+        
+                start_time = time.time()
+        
+                hist = model.fit(x_train, y_train, batch_size=batch, epochs=epoch, 
+                    verbose=False, validation_data=(x_val,y_val), callbacks=self.callbacks)
+        
+                duration = time.time() - start_time
+
+                model_acc = model.evaluate(x_val, y_val, verbose=False)[1]
+
+                if (model_acc > current_acc):
+                    IS_final = IS
+                    connect_final = connect
+                    x_train_final = x_train
+                    x_val_final = x_val
+                    y_train_final = y_train
+                    duration_final = duration
+                    model_final = model
+                    hist_final = hist
+                    #np.savetxt(self.output_dir+'IS.txt',IS_final)
+                    #np.savetxt(self.output_dir+'connectivity.txt', connect_final)
+                    current_acc = model_acc
+                
+                keras.backend.clear_session()
+        print('Final IS: {0}; Final connectivity: {1}'.format(IS_final, connect_final))
+        return x_train_final, x_val_final, y_train_final, model_final, hist_final, duration_final, current_acc 
+        
+    def fit(self, x_train, y_train, x_val, y_val, y_true):
+        #1. Tune ESN
+        x_train, x_val, y_train, model_init, hist_init, duration_init, acc_init = self.tune_esn(x_train, x_val, y_train, y_val)
+
+        current_acc = acc_init
+        hist_final = hist_init
+        model_final = model_init
+        duration_final = duration_init
+        ratio_final = [0.1,0.2]
+
+        
+        for ratio in self.ratio[1:]:
+
+            #1. Build Model
+            input_shape = (self.len_series, self.units, 1)
+            model = self.build_model(input_shape, self.nb_classes, self.len_series, ratio)
+            if(self.verbose==True):
+                model.summary()
+        
+        
+            #3. Train Model
+            batch = self.batch
+            epoch = self.epoch
+            
+            start_time = time.time()
+            
+            hist = model.fit(x_train, y_train, batch_size=batch, epochs=epoch,
+                verbose=False, validation_data=(x_val,y_val), callbacks=self.callbacks)
+            
+            duration = time.time() - start_time
+
+            model_acc = model.evaluate(x_val, y_val, verbose=False)[1]
+
+            if (model_acc > current_acc):
+                hist_final = hist
+                model_final = model
+                duration_final = duration
+                ratio_final = ratio
+                current_acc = model_acc
+            
+            keras.backend.clear_session()
+        print('Final ratio: {0}'.format(ratio_final))
+        self.model = model_final
+        self.hist = hist_final 
+
+        y_pred = self.model.predict(x_val)
+
+        # convert the predicted from binary to integer
+        y_pred = np.argmax(y_pred , axis=1)
+
+        save_logs(self.output_dir, self.hist, y_pred, y_true, duration_final, self.verbose, lr=False)
+
+        keras.backend.clear_session()
